efd9957c36a530754609f201b167fbc93de2152d
iambetheliz <bethelizganda22@gmail.com>
2017-04-26 10:01:23 +0800

    First Commit

--- administrator/dashboard.php
+++ administrator/dashboard.php
@@ -59,6 +59,7 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;<?php echo $userRow['userName'] ; ?>&nbsp;&nbsp;<span class="caret"></span></a>
                     <ul class="dropdown-menu">
+                        <li><a href="edit_profile.php">Change Password</a></li>
                         <li><a href="logout.php?logout">Logout</a></li>
                     </ul>
                 </li>
