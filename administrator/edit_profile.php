8b22089c09eb681d3bb12255b845d94f1d46de63
iambetheliz <bethelizganda22@gmail.com>
2017-04-28 13:12:40 +0800

    2nd Commit

--- administrator/edit_profile.php
+++ administrator/edit_profile.php
@@ -191,7 +191,7 @@
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;<?php echo $userRow['userName']; ?>&nbsp;&nbsp;<span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li>
-                            <a href="logout.php">Logout</a>
+                            <a href="logout.php?logout">Logout</a>
                         </li>
                     </ul>
                 </li>
